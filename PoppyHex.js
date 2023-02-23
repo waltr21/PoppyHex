9de11667498e6af3d9ec029288ef5413a0c1f937
WALTR <WALTR@DESKTOP-IH3G9NP>
2023-02-23 00:23:02 -0600

    First version. All basic logic.

---
+++ PoppyHex.js
@@ -0,0 +1,139 @@
+document.addEventListener("mouseup", selectionCheck);
+const colorReg = /^#([0-9a-fA-F]{3}){1,2}$/;
+injectHtml();
+let hovering = false;
+
+// No clue if this is the best way to do this!
+async function injectHtml(){
+  const rootEl = document.documentElement;
+  const url = browser.extension.getURL("PoppyHex.html");
+  var cssUrl = browser.extension.getURL("PoppyHex.css");
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = cssUrl;
+
+  // Inject the <link> element into the page's <head> section
+  document.head.appendChild(link);
+
+  const res = await fetch(url).then(function(response) {
+    return response.text();
+  });
+  const html = res.valueOf();
+  
+  rootEl.insertAdjacentHTML('beforeend', html);
+  setListeners();
+}
+
+function selectionCheck(){
+  let selection = "";
+  let activeEl = document.activeElement;
+  let activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+
+  // Handles text area: https://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
+  if (
+    (activeElTagName == "textarea") || (activeElTagName == "input" &&
+    /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+    (typeof activeEl.selectionStart == "number")
+  ) {
+    selection = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+  } else if (window.getSelection) {
+    selection = window.getSelection().toString();
+  }
+
+  checkSelectionForColor(selection.trim());
+}
+
+function checkSelectionForColor(str){
+  if (hovering) return; 
+  // Slap a # if missing.
+  if (str[0] != '#') str = `#${str}`;
+  let res = colorReg.exec(str);
+  const el = document.getElementById('poppy-container');
+  if (res){
+    setHex(str);
+    el.style.right = '20px';
+  } else {
+    closePopup();
+  }
+}
+
+function closePopup(){
+  if (!hovering){
+    const el = document.getElementById('poppy-container');
+    el.style.right = '-250px';
+  }
+}
+
+function setListeners(){
+  const copyHex = document.getElementById('copy-hex');
+  const copyNoHex = document.getElementById('copy-no-hex');
+  const copyRGB = document.getElementById('copy-rgb');
+  copyHex.addEventListener('click', function (event) {
+    copyToClip(copyHex.innerText);
+  });
+  copyNoHex.addEventListener('click', function (event) {
+    copyToClip(copyNoHex.innerText);
+  });
+  copyRGB.addEventListener('click', function (event) {
+    copyToClip(copyRGB.innerText);
+  });
+  
+  const poppyContainer = document.getElementById('poppy-container');
+  poppyContainer.addEventListener('mouseleave', function (event) {
+    setCopyHelper('Click to copy.');
+    hovering = false;
+  });
+
+  poppyContainer.addEventListener('mouseenter', function (event) {
+    hovering = true;
+  });
+}
+
+function copyToClip(str){
+  navigator.clipboard.writeText(str);
+  setCopyHelper('Copied!');
+}
+
+function setCopyHelper(str){
+  const el = document.getElementById('poppy-copy-helper');
+  el.innerText = str;
+}
+
+function setHex(str){
+  const swatch = document.getElementById('poppy-hex-title');
+  const swatchTitle = document.getElementById('poppy-color-swatch');
+  const copyHex = document.getElementById('copy-hex');
+  const copyNoHex = document.getElementById('copy-no-hex');
+  const copyRGB = document.getElementById('copy-rgb');
+  copyHex.innerText = str;
+  copyNoHex.innerText = str.replace('#', '');
+  copyRGB.innerText = hexToRgb(str);
+  swatchTitle.style.backgroundColor = str;
+  
+  // Always set title to 6 digits. Logic to include additional hash or not.
+  let res = hex3_to_hex6(str);
+  if (res.length == 6) res = `#${res}`;
+  swatch.innerText = res;
+}
+
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function hexToRgb(hex) {
+  hex = hex3_to_hex6(hex);
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const r = parseInt(result[1], 16);
+  const g = parseInt(result[2], 16);
+  const b = parseInt(result[3], 16);
+  return result ? `(${r},${g},${b})` : '(0,0,0)';
+}
+
+function hex3_to_hex6(str) {
+  const tempStr = str.replace('#', '');
+  if (tempStr.length != 3) return str;
+  let hex6 = "";
+  for (let i = 0; i < tempStr.length; i++) {
+    hex6 += tempStr[i] + tempStr[i];
+  }
+  return hex6;
+}
